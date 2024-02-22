a69433f87576e58bee4b0eea13bb3a29bcc535d7
javmunrom <javmunrom@alum.us.es>
2024-02-22 12:35:38 +0100

    Task-E4-02 #39 y Task-E4-03 #40 / Corrección de Sponsorship y creacion de Invoices.

--- src/main/java/acme/entities/Invoice.java
+++ src/main/java/acme/entities/Invoice.java
@@ -1,14 +1,13 @@
 
 package acme.entities;
 
-import java.time.Duration;
 import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
-import javax.validation.constraints.Email;
+import javax.persistence.ManyToOne;
+import javax.persistence.Transient;
+import javax.validation.Valid;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
@@ -18,14 +17,13 @@ import javax.validation.constraints.Pattern;
 import org.hibernate.validator.constraints.URL;
 
 import acme.client.data.AbstractEntity;
-import acme.datatypes.SponsorshipType;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
-public class Sponsorship extends AbstractEntity {
+public class Invoice extends AbstractEntity {
 
 	// Serialisation identifier -------------------------------------------------
 
@@ -34,30 +32,42 @@ public class Sponsorship extends AbstractEntity {
 	// Propieties ---------------------------------------------------------------
 
 	@NotBlank
-	@NotNull
-	@Pattern(regexp = "[A-Z]{1,3}-[0-9]{3}")
 	@Column(unique = true)
+	@Pattern(regexp = "IN-[0-9]{4}-[0-9]{4}")
 	private String				code;
 
 	@NotNull
 	@Past
-	@Temporal(TemporalType.DATE)
-	private Date				moment;
+	private Date				registrationTime;
 
 	@NotNull
-	private Duration			duration;
+	private Date				dueDate;
 
 	@NotNull
 	@Min(0)
-	private Double				amount;
+	private int					quantity;
 
 	@NotNull
-	private SponsorshipType		type;
-
-	@Email
-	private String				email;
+	@Min(0)
+	private Double				tax;
 
 	@URL
 	private String				link;
+
+	// Derived attributes -------------------------------------------------------
+
+
+	@Transient
+	public Double getTotalAmount() {
+		return this.tax + this.quantity;
+	}
+
+	// Relationships
+
+
+	@Valid
+	@NotNull
+	@ManyToOne(optional = true)
+	private Sponsorship sponsorship;
 
 }
