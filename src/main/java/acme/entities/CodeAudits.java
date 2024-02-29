6dfe3075533a6b60d783defc50979e1572e12ae6
alvaro-us <alvparbor1@alum.us.es>
2024-02-29 18:48:58 +0100

    TaskE5-032 - Code audits: desarrollo completo

---
+++ src/main/java/acme/entities/CodeAudits.java
@@ -0,0 +1,53 @@
+
+package acme.entities;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.client.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+
+public class CodeAudits extends AbstractEntity {
+
+	// Serialisation identifier -----------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@NotBlank
+	@Column(unique = true)
+	@Pattern(regexp = "[A-Z]{1,3}-[0-9]{3}")
+	private String				code;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@Past
+	private Date				executionDate;
+
+	protected EnumType			type;
+
+	@NotBlank
+	@Length(min = 1, max = 102)
+	private List<String>		proposedCorrectiveActions;
+
+	private Double				mark;
+
+	@URL
+	private String				link;
+}
