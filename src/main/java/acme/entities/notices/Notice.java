2cd1d23a79c22a5e7464fbaca8b2295d7b77d550
pablo_absqwdw <pablo_absqwdw@dircom.manten.us.es>
2024-03-05 15:59:50 +0100

    Task-G-6: Entrega requisito 6

---
+++ src/main/java/acme/entities/notices/Notice.java
@@ -0,0 +1,53 @@
+
+package acme.entities.notices;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.client.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Notice extends AbstractEntity {
+	// Serialisation Identifier
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+	@Temporal(TemporalType.TIMESTAMP)
+	@Past
+	@NotNull
+	private Date				instMoment;
+
+	@NotBlank
+	@Length(max = 75)
+	private String				title;
+
+	@NotBlank
+	@Length(max = 75)
+	private String				author;
+
+	@NotBlank
+	@Length(max = 100)
+	private String				message;
+
+	@Email
+	private String				email;
+
+	@URL
+	private String				link;
+
+}
