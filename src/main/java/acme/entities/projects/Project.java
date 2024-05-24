6018f79977dd21ccd17d4a34bf695bb8b0345175
Pablo Cuenca <pabcueper@alum.us.es>
2024-05-24 15:28:20 +0200

    Testeos actualizados

--- src/main/java/acme/entities/projects/Project.java
+++ src/main/java/acme/entities/projects/Project.java
@@ -3,9 +3,7 @@ package acme.entities.projects;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.Index;
 import javax.persistence.ManyToOne;
-import javax.persistence.Table;
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
@@ -23,10 +21,6 @@ import lombok.Setter;
 @Getter
 @Setter
 @Entity
-@Table(indexes = {
-	@Index(columnList = "code"), @Index(columnList = "code, id")
-})
-
 public class Project extends AbstractEntity {
 
 	// Serialisation Identifier
